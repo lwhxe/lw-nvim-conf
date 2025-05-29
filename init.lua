c3d211cbb75b62372dfe124c4960fa9f9170b95e
UrLastWish <149725738+lwhxe@users.noreply.github.com>
2025-05-30 00:15:47 +0200

    autoupdate lazy.nvim plugins

--- init.lua
+++ init.lua
@@ -2,4 +2,9 @@ if vim.fn.executable("%appdata%/../Local/nvim/UPDATE.bat") == 1 then
     os.execute("%appdata%/../Local/nvim/UPDATE.bat")
 end
 
+os.execute("cd C:/")
+os.execute("mkdir telekasten")
+
 require("lwhxe")
+
+vim.api.nvim_create_autocmd("VimEnter",{callback=function()require"lazy".update({show = false})end})
